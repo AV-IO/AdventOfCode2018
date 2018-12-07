d7a46166a3b4486bf3fdc4b8c7762f9a1429b963
Ian Guile <ianguile513@gmail.com>
2018-12-06 19:49:17 -0700

    updated Day 03

--- 03/challenge.go
+++ 03/challenge.go
@@ -7,17 +7,20 @@ import (
 	s "strings"
 )
 
-// ReadInstructions :
-//  	parameters:
-//  		instructions: instructions for requested squares of fabric
+//	ReadInstructions :
+//		parameters:
+//			instructions: instructions for requested squares of fabric
 //		return values:
-//  		sharedIn: the number of shared square inches
-func ReadInstructions(instructions []string) (sharedIn int, lonePatch int) {
-	var fabric [1000][1000]int
-	fab := map[[2]int][]int{}
-	fab[[2]int{2, 2}] = []int{4}
+//			sharedIn: the number of shared square inches
+//			intactPatch:
+func ReadInstructions(instructions []string) (sharedIn int, intactPatch int) {
+	fabric := map[[2]int][]int{}
+	intactPatches := make(map[int]bool)
+	for i := 0; i < len(instructions); i++ {
+		intactPatches[i+1] = false
+	}
 
-	for _, inst := range instructions {
+	for instN, inst := range instructions {
 		i := s.Split(inst, " ")
 		xOff, _ := sc.Atoi(i[2][:s.Index(i[2], ",")])
 		yOff, _ := sc.Atoi(i[2][s.Index(i[2], ",")+1 : len(i[2])-1])
@@ -26,9 +29,14 @@ func ReadInstructions(instructions []string) (sharedIn int, lonePatch int) {
 
 		for x := 0; x < xSize; x++ {
 			for y := 0; y < ySize; y++ {
-				fabric[xOff+x][yOff+y]++
-				if fabric[xOff+x][yOff+y] == 2 {
+				coord := [2]int{xOff + x, yOff + y} // Go: MaP vAlUeS aReN't AdDrEsSaBlE wItH pOiNtErS
+				fabric[coord] = append(fabric[coord], instN)
+				if len(fabric[coord]) == 2 {
 					sharedIn++
+					delete(intactPatches, fabric[coord][0])
+					delete(intactPatches, fabric[coord][1])
+				} else if len(fabric[coord]) > 2 {
+					delete(intactPatches, fabric[coord][len(fabric[coord])])
 				}
 			}
 		}
@@ -39,8 +47,8 @@ func ReadInstructions(instructions []string) (sharedIn int, lonePatch int) {
 
 func main() {
 	data, _ := ioutil.ReadFile("./input")
-	sharedIn := ReadInstructions(s.Split(string(data), "\r\n"))
-	output := "shared inches: " + sc.Itoa(sharedIn) + "\n"
+	sharedIn, intactPatch := ReadInstructions(s.Split(string(data), "\r\n"))
+	output := "shared inches: " + sc.Itoa(sharedIn) + "\nintact Patch: " + sc.Itoa(intactPatch) + "\n"
 	fmt.Println(output)
 	ioutil.WriteFile("./output", []byte(output), 0644)
 }
--- 03/challenge_test.go
+++ 03/challenge_test.go
@@ -4,16 +4,21 @@ import "testing"
 
 func Test_ReadInstructions(t *testing.T) {
 	tests := []struct {
-		name         string
-		instructions []string
-		wantsharedIn int
+		name            string
+		instructions    []string
+		wantsharedIn    int
+		wantintactPatch int
 	}{
-		{"example 1-1", []string{"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"}, 4},
+		{"example 1-1", []string{"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"}, 4, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotsharedIn := ReadInstructions(tt.instructions); gotsharedIn != tt.wantsharedIn {
+			gotsharedIn, gotintactPatch := ReadInstructions(tt.instructions)
+			if gotsharedIn != tt.wantsharedIn {
 				t.Errorf("ReadInstructions() sharedIn: got %v, wanted %v", gotsharedIn, tt.wantsharedIn)
+			}
+			if gotintactPatch != tt.wantintactPatch {
+				t.Errorf("ReadInstructions() intactPatch: got %v, wanted %v", gotintactPatch, tt.wantintactPatch)
 			}
 		})
 	}
