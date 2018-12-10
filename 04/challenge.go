b20380e3bd9e08efb377df1e5d8e9e6920c30809
AV-IO <ianguile513@gmail.com>
2018-12-10 02:43:37 -0700

    Finished Day 04 part 2

--- 04/challenge.go
+++ 04/challenge.go
@@ -8,12 +8,12 @@ import (
 	s "strings"
 )
 
-// funcName :
+// findSleepTime :
 //  	parameters:
 //  		rawLogs: list of unparsed logs with timestamps and what action was performed
 //  	return values:
-//  		retA: description
-func funcName(rawLogs []string) (retA int) {
+//  		mostAsleep: description
+func findSleepTime(rawLogs []string) (mostAsleep int, frequentMinute int) {
 	sort.Strings(rawLogs)
 
 	// parsing logs into usable structure
@@ -26,9 +26,9 @@ func funcName(rawLogs []string) (retA int) {
 
 	// finding how much time each guard spent sleeping
 	// Somewhat state-machine like
-	// This could probably be better done by traversing lines up and down, but I want to do it this way
+	// This could probably be better done by traversing lines up and down
 	// Maybe this would be solved better more functionally
-	guardTimes := make(map[string][]int)
+	guardTimes := make(map[string]map[int]int)
 	currentGuard := ""
 	time := 0
 	for _, l := range parsedLogs {
@@ -40,37 +40,54 @@ func funcName(rawLogs []string) (retA int) {
 		case "wakes":
 			awakeTime, _ := sc.Atoi(l[1][3:])
 			for ; time < awakeTime; time++ {
-				guardTimes[currentGuard] = append(guardTimes[currentGuard], time)
+				if _, guardExists := guardTimes[currentGuard]; guardExists { // if guard exists
+					guardTimes[currentGuard][time]++
+				} else {
+					guardTimes[currentGuard] = map[int]int{time: 1}
+				}
 			}
 		}
 	}
 
 	// finding which guard spent the most time sleeping
 	time = 0
-	for key, val := range guardTimes {
-		if len(val) > time {
-			currentGuard = key
-			time = len(val)
+	frequentCount := 0
+	for guard, timeMap := range guardTimes {
+		totalTime := 0
+		for minute, timeCount := range timeMap {
+			totalTime += timeCount         // pt. 1
+			if timeCount > frequentCount { // pt. 2
+				guardID, _ := sc.Atoi(guard[1:])
+				frequentMinute = minute * guardID
+				frequentCount = timeCount
+			}
+		}
+		if totalTime > time {
+			currentGuard = guard
+			time = totalTime
 		}
 	}
 
 	// finding most common hour asleep
-	sort.Ints(guardTimes[currentGuard])
-	for i, _ := range guardTimes[currentGuard] {
-
-		guardTimes[currentGuard][i]
+	time = 0
+	for minute, count := range guardTimes[currentGuard] {
 		if count > time {
+			mostAsleep = minute
 			time = count
 		}
 	}
+
+	//
+	guardID, _ := sc.Atoi(currentGuard[1:])
+	mostAsleep *= guardID
 
 	return
 }
 
 func main() {
 	data, _ := ioutil.ReadFile("./input")
-	retA := funcName(s.Split(string(data), "\n"))
-	output := "retA: " + sc.Itoa(retA) + "\n"
+	mostAsleep, frequentMinute := findSleepTime(s.Split(string(data), "\n"))
+	output := "most time asleep: " + sc.Itoa(mostAsleep) + "\nmost frequently asleep minute: " + sc.Itoa(frequentMinute) + "\n"
 	fmt.Println(output)
 	ioutil.WriteFile("./output", []byte(output), 0644)
 }
