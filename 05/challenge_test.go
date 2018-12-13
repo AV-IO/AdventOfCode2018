8f9f12ac1f65aeada52042f395ac6a1d832e5d8a
Ian Guile <ianguile513@gmail.com>
2018-12-13 14:43:25 -0700

    updated test file

--- 05/challenge_test.go
+++ 05/challenge_test.go
@@ -1,32 +1,70 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAlchemicalReduction(t *testing.T) {
+	type parameters struct {
+		inPolymer  string
+		outPolyLen chan int
+	}
 	tests := []struct {
-		name              string
-		inPolymer         string
-		wantOutPolymerStr string
-		wantOutPolymerLen int
+		name        string
+		param       parameters
+		wantPolyLen int
 	}{
-		{"example 1-1", "aA", "", 0},
-		{"example 1-2", "abBA", "", 0},
-		{"example 1-3", "abAB", "abAB", 4},
-		{"example 1-4", "aabAAB", "aabAAB", 6},
-		{"example 1-5", "dabAcCaCBAcCcaDA", "dabCBAcaDA", 10},
+		{"example 1-1", parameters{"aA", make(chan int)}, 0},
+		{"example 1-2", parameters{"abBA", make(chan int)}, 0},
+		{"example 1-3", parameters{"abAB", make(chan int)}, 4},
+		{"example 1-4", parameters{"aabAAB", make(chan int)}, 6},
+		{"example 1-5", parameters{"dabAcCaCBAcCcaDA", make(chan int)}, 10},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// TODO: this is old. needs to be updated to match channel usage
-			gotOutPolymer := AlchemicalReduction(tt.inPolymer)
-			gotOutPolymerLen := len(gotOutPolymer)
-			if gotOutPolymer != tt.wantOutPolymerStr {
+			AlchemicalReduction(tt.param.inPolymer, tt.param.outPolyLen)
+			if gotPolyLen := <-tt.param.outPolyLen; gotPolyLen != tt.wantPolyLen {
 				t.Errorf(
-					"AlchemicalReduction() outPolymer: got %v (len: %v), wanted %v (len: %v)",
-					gotOutPolymer,
-					gotOutPolymerLen,
-					tt.wantOutPolymerStr,
-					tt.wantOutPolymerLen,
+					"AlchemicalReduction() outPolyLen: got %v, wanted %v)",
+					gotPolyLen,
+					tt.wantPolyLen,
+				)
+			}
+		})
+	}
+}
+
+func TestFindProblemUnit(t *testing.T) {
+	type parameters struct {
+		inPolymer       string
+		ProblemUnit     chan string
+		outFixedPolyLen chan int
+	}
+	tests := []struct {
+		name        string
+		param       parameters
+		wantProblem string
+		wantPolyLen int
+	}{
+		{"example 2-1", parameters{"dabAcCaCBAcCcaDA", make(chan string), make(chan int)}, "cC", 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			FindProblemUnit(tt.param.inPolymer, tt.param.ProblemUnit, tt.param.outFixedPolyLen)
+			gotProblem := <-tt.param.ProblemUnit
+			gotPolyLen := <-tt.param.outFixedPolyLen
+			if gotProblem != tt.wantProblem {
+				t.Errorf(
+					"FindProblemUnit() ProblemUnit: got %v, wanted %v",
+					gotProblem,
+					tt.wantProblem,
+				)
+			}
+			if gotPolyLen != tt.wantPolyLen {
+				t.Errorf(
+					"FindProblemUnit() outFixedPolyLen: got %v, wanted %v",
+					gotPolyLen,
+					tt.wantPolyLen,
 				)
 			}
 		})
