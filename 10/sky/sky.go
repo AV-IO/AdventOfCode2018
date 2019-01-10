964e6734adf946a422c2f883831f0202099a3143
AV-IO <ianguile513@gmail.com>
2019-01-10 00:09:13 -0700

    progress?

--- 10/sky/sky.go
+++ 10/sky/sky.go
@@ -15,24 +15,24 @@ type Vector struct {
 // Sky contains a slice of vectors, along with min/max positions for the slice's initial state
 type Sky struct {
 	Field []Vector
-	minX  int
-	minY  int
-	maxX  int
-	maxY  int
+	MinX  int
+	MinY  int
+	MaxX  int
+	MaxY  int
 }
 
 // PassTime moves each vector based on how many seconds have passed
 func (s *Sky) PassTime(seconds int) {
 	for _, v := range s.Field {
-		if v.PositionX = seconds * v.VelocityX; v.PositionX < s.minX {
-			s.minX = v.PositionX
-		} else if v.PositionX > s.maxX {
-			s.maxX = v.PositionX
+		if v.PositionX = seconds * v.VelocityX; v.PositionX < s.MinX {
+			s.MinX = v.PositionX
+		} else if v.PositionX > s.MaxX {
+			s.MaxX = v.PositionX
 		}
-		if v.PositionY = seconds * v.VelocityY; v.PositionY < s.minY {
-			s.minY = v.PositionY
-		} else if v.PositionY > s.maxY {
-			s.maxY = v.PositionY
+		if v.PositionY = seconds * v.VelocityY; v.PositionY < s.MinY {
+			s.MinY = v.PositionY
+		} else if v.PositionY > s.MaxY {
+			s.MaxY = v.PositionY
 		}
 	}
 }
@@ -43,31 +43,31 @@ func (s *Sky) AddVector(v Vector) {
 	s.Field = append(s.Field, v)
 
 	// modify sky min/max values as necessary
-	if v.PositionX < s.minX {
-		s.minX = v.PositionX
-	} else if v.PositionX > s.maxX {
-		s.maxX = v.PositionX
+	if v.PositionX < s.MinX {
+		s.MinX = v.PositionX
+	} else if v.PositionX > s.MaxX {
+		s.MaxX = v.PositionX
 	}
-	if v.PositionY < s.minY {
-		s.minY = v.PositionY
-	} else if v.PositionY > s.maxY {
-		s.maxY = v.PositionY
+	if v.PositionY < s.MinY {
+		s.MinY = v.PositionY
+	} else if v.PositionY > s.MaxY {
+		s.MaxY = v.PositionY
 	}
 }
 
 // ToString returns a string displaying the current sky
 func (s *Sky) ToString() (str string) {
-	//strArr := make([]string, (s.maxX-s.minX)*(s.maxY-s.minY))
-	strArr := make([][]string, s.maxY-s.minY)
-	for i := 0; i < s.maxY-s.minY; i++ {
-		strArr[i] = make([]string, s.maxX-s.minX)
+	//strArr := make([]string, (s.MaxX-s.MinX)*(s.MaxY-s.MinY))
+	strArr := make([][]string, s.MaxY-s.MinY)
+	for i := 0; i < s.MaxY-s.MinY; i++ {
+		strArr[i] = make([]string, s.MaxX-s.MinX)
 	}
 
 	for _, v := range s.Field {
-		strArr[v.PositionX+s.minX][v.PositionY+s.minY] = "*"
+		strArr[v.PositionX+s.MinX][v.PositionY+s.MinY] = "*"
 	}
 
-	for i := 0; i < s.maxY-s.minY; i++ {
+	for i := 0; i < s.MaxY-s.MinY; i++ {
 		str += strings.Join(strArr[i], "")
 	}
 
